005715f5c99075d3b9fe38740d04437440106989
israelb <israelbinunsky@gmail.com>
2025-05-14 16:28:33 +0300

    the func

--- Program.cs
+++ Program.cs
@@ -11,5 +11,29 @@ namespace atbash
         static void Main(string[] args)
         {
         }
+
+        static string atbash(string txt)
+        {
+            char[] chars = {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g',
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+    'x', 'y', 'z'};
+            char[] reverse = new char[chars.Length];
+            Dictionary<char, char> map = new Dictionary<char, char>();
+            string newtxt = "";
+
+            for (int i = 0; i < chars.Length; i++)
+            { reverse[i] = chars[i]; }
+
+            for (int j = 0; j < chars.Length; j++)
+            { map[chars[j]] = reverse[j]; }
+
+            foreach (char c in txt)
+                newtxt += map[c];
+            
+            return newtxt;
+            }
+        }
     }
-}
+
